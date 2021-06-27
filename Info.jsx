80fdd9c2422f49928003e8fe86af19ef30ed1e8a
JasinthaNanthakumar <86550940+JasinthaNanthakumar@users.noreply.github.com>
2021-06-27 20:00:48 +0530

    Update Info.jsx

--- Info.jsx
+++ Info.jsx
@@ -4,7 +4,7 @@ function Info() {
   return (
     <div className="note">
       <h1>Javascript and React.js</h1>
-      <p>vasic wev dev React Js Bootcamp</p>
+      <p>Basic wev dev React Js Bootcamp</p>
     </div>
   );
 }
